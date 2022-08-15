89adf7657c7d1eeb45cd8bca00e73c844ed02634
Shubhankar Shandilya <78155393+shubhankar-shandilya-india@users.noreply.github.com>
2022-08-15 18:48:23 +0530

    Add files via upload

---
+++ Remove Element from Array.cpp
@@ -0,0 +1,11 @@
+int Solution::removeElement(vector<int> &A, int B) {
+   int i = 0;
+    for (int j = 0; j < A.size(); j++) {
+        if (A[j] != B) {
+            A[i] = A[j];
+            i++;
+        }
+    }
+    return i;
+}
+
