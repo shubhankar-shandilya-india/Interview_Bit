7b90cca3423cba43943ed39102c51d0ddafff940
Shubhankar Shandilya <78155393+shubhankar-shandilya-india@users.noreply.github.com>
2022-08-18 19:26:37 +0530

    Update Intersection Of Sorted Arrays_correct.cpp

--- Intersection Of Sorted Arrays_correct.cpp
+++ Intersection Of Sorted Arrays_correct.cpp
@@ -3,3 +3,24 @@ vector<int> Solution::intersect(const vector<int> &A, const vector<int> &B) {
     set_intersection(A.begin(),A.end(),B.begin(),B.end(),back_inserter(ans));
     return ans;
 }
+
+
+vector<int> Solution::intersect(const vector<int> &A, const vector<int> &B) {
+    int n = A.size() , m = B.size();
+    vector<int>inter;
+    int i = 0 , j = 0;
+    while( i < n && j < m)
+    {
+        if( A[i] == B[j])
+        {
+            inter.push_back(A[i]);
+            i++,j++;
+        }
+        else if(A[i] < B[j])
+        i++;
+        else
+        j++;
+    }
+    return inter;
+}
+
